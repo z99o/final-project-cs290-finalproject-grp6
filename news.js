d1d4250b50ec08e951f0c2bb56fce9566a755f1e
ivantey109 <49384181+ivantey109@users.noreply.github.com>
2019-05-28 15:37:40 -0700

    Add files via upload

---
+++ news.js
@@ -0,0 +1,16 @@
+var get_commend = document.getElementById('add-commend');
+var button = document.getElementById('commend-button-id');
+console.log(button);
+
+
+/*
+button.addEventListener('click',function(){
+  document.getElementById('twit-commend').className = 'visible';
+  console.log('clicked');
+});
+*/
+
+button.onclick = function(){
+  document.getElementById('add-commend').className = 'visible';
+};
+
