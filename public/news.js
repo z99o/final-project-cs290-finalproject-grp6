55276e1f548965bfb7fa33e8062ec30aead14e98
Ryan Little <ryan.jay.little@gmail.com>
2019-06-06 17:40:04 -0700

    Hide Post Icons

--- public/news.js
+++ public/news.js
@@ -1,54 +1,50 @@
-
 // Handle user's clicks
-window.addEventListener('click', function(event)
-{
+window.addEventListener('click', function(event) {
 
   // When the user clicks the post button
   // (doesn't do anything yet)
-  if(event.target.classList.contains("accept-button"))
-  {
+  if (event.target.classList.contains("accept-button")) {
     console.log("post button clicked");
   }
 
   // When the user clicks the see comments button
-  else if(event.target.classList.contains("view-comments-button"))
-  {
+  else if (event.target.classList.contains("view-comments-button")) {
     viewComments(event);
   }
-  else if(event.target.classList.contains("hide-post-button"))
-  {
+
+  // If the user clicks the hide post button or the icon in the button
+  else if (event.target.classList.contains("hide-post-button") ||
+    event.target.parentNode.classList.contains("hide-post-button")) {
     hidePost(event);
   }
 });
 
 // Shows/hides comments of the post that the user clicked on
-function viewComments(event)
-{
+function viewComments(event) {
   var post = event.target.parentNode.parentNode;
   var comments = post.getElementsByClassName("comment-container")[0];
   comments.classList.toggle("hidden");
 }
 
 
-function hidePost(event)
-  {
-    var post = event.target.parentNode;
-    var comments = post.getElementsByClassName("post-content")[0];
-    comments.classList.toggle("hidden");
+function hidePost(event) {
+  var button;
+  // Gets the button element regardless of whether the user clicked
+  // the icon or the button
+  event.target.parentNode.classList.contains("hide-post-button") ?
+    button = event.target.parentNode : button = event.target;
 
-    if(event.target.innerHTML =="v")
-    {
-      event.target.innerHTML ="&#652";
-    }
-    else
-    {
-      event.target.innerHTML ="v";
-    }
-  }
+  var post = button.parentNode;
+  var comments = post.getElementsByClassName("post-content")[0];
+  comments.classList.toggle("hidden");
+  var icon = button.firstChild;
+
+  icon.classList.toggle("fa-chevron-up");
+  icon.classList.toggle("fa-chevron-down");
+}
 
 
 var menu = document.getElementById('sideMenu');
-menu.addEventListener('click', function(){
+menu.addEventListener('click', function() {
   document.getElementById("background").style.background = "lightblue";
 })
-
