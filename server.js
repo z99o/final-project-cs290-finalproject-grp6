79c880bc6d26330b2b2797067ceb7c49dce963a1
z99o <z99o@users.noreply.github.com>
2019-06-08 20:27:16 -0700

    Connects to mongo now

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var http = require('http');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
-
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var port = process.env.PORT || 3000;
+
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = "cs290_morelloz";
+var mongoDBName = process.env.MONGO_DB_NAME;
+
+var mongoURL ='mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
@@ -15,7 +24,7 @@ app.use(express.static('public'));
 app.get('/',function(req,res,next){
     console.log("==Server Displaying Index");
     res.status(200).render('news',{
-        
+
     });
 });
 
@@ -24,6 +33,12 @@ app.get('*',function(req,res,next){
     res.status(404);
 });
 
-app.listen(port, function () {
-    console.log("== Server is listening on port", port);
+MongoClient.connect(mongoURL,function(err,client){
+    if (err) {
+        throw err;
+    }
+    db = mongoDBDatabase = client.db(mongoDBName);
+    app.listen(port, function () {
+        console.log("== Server is listening on port", port);
+    });
 });
