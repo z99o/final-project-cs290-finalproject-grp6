8b772bfff92fd4acdf0656d40cd7b6e17d06c4fa
Ivan <ivantey@10-248-204-206.wireless.oregonstate.edu>
2019-06-13 14:54:24 -0700

    css 404 and header

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var port = process.env.PORT || 3000;
 
-var mongoHost = process.env.MONGO_HOST;
+var mongoHost = "classmongo.engr.oregonstate.edu";
 var mongoPort = process.env.MONGO_PORT || 27017;
-var mongoUser = process.env.MONGO_USER;
-var mongoPassword = process.env.MONGO_PASSWORD;
-var mongoDBName = process.env.MONGO_DB_NAME;
+var mongoUser = "cs290_morelloz";
+var mongoPassword = "BigDatabaseEnergy";
+var mongoDBName = "cs290_morelloz";
 
 var mongoURL ='mongodb://' + mongoUser + ':' + mongoPassword + '@' + mongoHost + ':' + mongoPort + '/' + mongoDBName;
 
